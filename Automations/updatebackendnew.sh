fd9b92a233dbc2c64944b148ec0500d1e0650f39
Prajwal1414 <prajwalphs@gmail.com>
2025-09-25 20:36:00 +0530

    Cleaned up automation scripts

--- Automations/updatebackendnew.sh
+++ Automations/updatebackendnew.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 
 # Set instance ID
-INSTANCE_ID = "i-3214dsf234f"
+INSTANCE_ID="i-3214dsf234f"
 
 # Get the public ip of the instance specified
-ipv4_address = $(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query'Reservations[0].Instances[0].PublicIpAddress' --output text)
+ipv4_address=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query 'Reservations[0].Instances[0].PublicIpAddress' --output text)
 
 # Path to .env.docker
-file_to_find = ../backend/.env.docker
+file_to_find=../backend/.env.docker
 
 # Check the current frontendurl in the env
-current_url = $(sed -n "4p" $file_to_find)
+current_url=$(sed -n "4p" "$file_to_find")
 
 # Update the env if current_url and the frontendurl in env don't match
-if [["$current_url" != "FRONTEND_URL=\"http://${ipv4_address}:5173\""]]; then
-  if [-f $file_to_find]; then
+if [[ "$current_url" != "FRONTEND_URL=\"http://${ipv4_address}:5173\"" ]]; then
+  if [ -f "$file_to_find" ]; then
     sed -i -e "s|FRONTEND_URL.*|FRONTEND_URL=\"http://${ipv4_address}:5173\"|g" $file_to_find
   else
     echo "ERROR: File not found"
